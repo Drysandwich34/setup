79cdf23f74d2e867e040a75c7d4a7773b85ee3f0
Drysandwich34 <rodriguezspencer11@gmail.com>
2026-09-05 21:35:05 -0600

    Add Zed settings sync to setup
    Track zed/settings.json and zed/keymap.json; install them in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,28 @@ chmod u+x install.sh
 ./install.sh --help
 
 ./install.sh install FiraCode
+
+curl https://github.com/Drysandwich34/setup/raw/refs/heads/main/dms-backup-20260905-205533.tar.gz > dms.tar.gz
+
+
+sudo dnf install adw-gtk3-theme -y
+sudo dnf install qt6ct-kde -y
+
+mkdir -p ~/.config/environment.d
+tee ~/.config/environment.d/qt-theme.conf > /dev/null <<'EOF'
+QT_QPA_PLATFORMTHEME=qt6ct
+EOF
+
+mkdir -p ~/.config/zed
+curl -fsSL https://raw.githubusercontent.com/Drysandwich34/setup/main/zed/settings.json -o ~/.config/zed/settings.json
+curl -fsSL https://raw.githubusercontent.com/Drysandwich34/setup/main/zed/keymap.json -o ~/.config/zed/keymap.json
+
+sudo dnf install zsh -y
+
+sh -c "$(curl -fsSL https://githubusercontent.com)" "" --unattended
+
+sudo chsh -s $(which zsh) $USER
+
+curl https://raw.githubusercontent.com/typecraft-dev/dotfiles/refs/heads/master/starship/.config/starship.toml > ~/.config/starship.toml
+
+git clone https://github.com/Drysandwich34/poovim.git ~/.config/nvim
