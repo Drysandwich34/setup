35aa2aaa921a392e1d2a9d32688c789f94c6c562
Drysandwich34 <rodriguezspencer11@gmail.com>
2026-09-05 21:03:05 -0600

    added .

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo -v && curl -fsSL https://install.danklinux.com | sh -s -- -c hyprland -t ghostty -y --danksearch --replace-configs hyprland
+
+
+curl -s https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh -o install.sh
+chmod u+x install.sh
+./install.sh --help
+
+./install.sh install FiraCode
